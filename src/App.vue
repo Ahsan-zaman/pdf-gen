76ad579a62b5cc690b502cbb83bef15ec177a2d6
Ahsan <12345jitu67890@gmail.com>
2019-11-06 15:54:51 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,150 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+	<input type="text" name="" v-model="name" id="">
+	<input type="text" name="" v-model="CourseName" id="">
+	<input type="text" name="" v-model="date" id="">
+	<div class="button" @click="gen">generate pdf</div>
+	<div id="app">
+		<div style="width:842px; height:595px; padding:20px; text-align:center;margin:auto; border: 10px solid #787878">
+		<div style="width:792px; height:545px; padding:20px; text-align:center; border: 5px solid #787878">
+				<br><br><br><br>
+				<span style="font-size:50px; font-weight:bold">Certificate of Completion</span>
+				<br><br>
+				<span style="font-size:25px"><i>This is to certify that</i></span>
+				<br><br>
+				<span style="font-size:30px"><b>{{name}}</b></span><br/><br/>
+				<span style="font-size:25px"><i>has completed the course</i></span> <br/><br/>
+				<span style="font-size:30px">{{CourseName}}</span> <br/><br/>
+				<span style="font-size:20px">with score of <b>99%</b></span> <br/><br/><br/>
+				<span style="font-size:25px"><i>dated</i></span><br>
+				{{date}}<br>
+				<!-- <span style="font-size:30px">$dt</span> -->
+		</div>
+		</div>
+	</div>
+	
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import jsPDF from 'jspdf'
+import axios from 'axios'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	// components: {
+	// 	HelloWorld
+	// },
+	data(){
+		return{
+			name:'محمد احسان الزمان',
+			CourseName: 'Course Name',
+			date: new Date().toISOString().substr(0,10),
+		}
+	},
+	methods:{
+		gen(){
+			var divContents = document.getElementById('app').innerHTML;
+			// var printWindow = window.open('', '', 'height=400,width=800');
+			// printWindow.document.write('<html><head><title>DIV Contents</title>');
+			// printWindow.document.write('</head><body >');
+			// printWindow.document.write(divContents);
+			// printWindow.document.write('</body></html>');
+			// printWindow.document.close();
+			// printWindow.print();
+
+
+			// let pdfName = 'test'; 
+			// var doc = new jsPDF();
+			// doc.text(divContents, 10, 10);
+			// doc.save(pdfName + '.pdf');
+			// var printDoc = new jsPDF();
+			// printDoc.fromHTML(document.getElementById('app').innerHTML, 15, 15, {
+			// 	'width': 170,
+			// });
+			// printDoc.save('sample-file.pdf');
+			// printDoc.fromHTML(divContents, 10, 10, {'width': 180});
+			// printDoc.autoPrint();
+			// printDoc.output("dataurlnewwindow");
+			var endpoint = "https://v2018.api2pdf.com/chrome/html"
+			var apiKey = "4d6c0a01-bf2d-45a6-afab-70a315cf26a7"
+			var config = {
+				headers: {
+				Authorization: apiKey
+				}
+			}
+			var content = '<html><head><meta charset="UTF-8"></head><body style="background: #f1f1f1;background-size: cover;">' + divContents + '</body></html>'
+			var payload = {
+				html: content, //Use your own HTML
+				inlinePdf: true,
+				options: {
+					landscape : true
+				}
+			}
+			axios.post(endpoint, payload, config)
+			.then(function(response) {
+				window.open(response.data.pdf)
+				// alert(response.data.pdf); //this is your PDF! Do something with it
+			})
+			.catch(function(error) {
+				alert(error);
+			});
+		},
+		// gendl(){
+		// 	var divContents = document.getElementById('app').innerHTML;
+		// 	var endpoint = "https://v2018.api2pdf.com/chrome/html"
+		// 	var apiKey = "4d6c0a01-bf2d-45a6-afab-70a315cf26a7"
+		// 	var config = {
+		// 		headers: {
+		// 		Authorization: apiKey
+		// 		}
+		// 	}
+		// 	var content = '<html><head><meta charset="UTF-8"></head><body>' + divContents + '</body></html>'
+		// 	var payload = {
+		// 		html: content, //Use your own HTML
+		// 		inlinePdf: true,
+		// 		options: {
+		// 			landscape : true
+		// 		}
+		// 	}
+		// 	axios.post(endpoint, payload, config)
+		// 	.then(function(response) {
+		// 		var link = document.createElement("a");
+		// 		link.href = response.data.pdf;
+		// 		link.setAttribute("download");
+		// 		link.click();
+		// 		document.body.removeChild(link);
+		// 		// delete link;
+		// 	})
+		// 	.catch(function(error) {
+		// 		alert(error);
+		// 	});
+		// }
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+input{
+	font-size: 1.2rem;
+	padding: 10px
+}
+.button{
+	display: inline;
+	text-transform: uppercase;
+	padding: 10px 20px;
+	margin-left: 50px;
+	background-color: #2c3e50;
+	color: #fff;
+	cursor: pointer;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,21 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  
+<div style="width:842px; height:595px; padding:20px; text-align:center; border: 10px solid #787878">
+  <div style="width:792px; height:545px; padding:20px; text-align:center; border: 5px solid #787878">
+        <br><br><br><br>
+        <span style="font-size:50px; font-weight:bold">Certificate of Completion</span>
+        <br><br>
+        <span style="font-size:25px"><i>This is to certify that</i></span>
+        <br><br>
+        <span style="font-size:30px"><b>محمد احسان الزمان</b></span><br/><br/>
+        <span style="font-size:25px"><i>has completed the course</i></span> <br/><br/>
+        <span style="font-size:30px">CourseName</span> <br/><br/>
+        <span style="font-size:20px">with score of <b>99%</b></span> <br/><br/><br/>
+        <span style="font-size:25px"><i>dated</i></span><br>
+        {{new Date().toISOString().substr(0,10)}}<br>
+        <!-- <span style="font-size:30px">$dt</span> -->
   </div>
+</div>
 </template>
 
 <script>
